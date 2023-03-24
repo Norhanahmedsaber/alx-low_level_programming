f6e294d4f47a562dbe5d041e2babc65998123c2e
Norhanahmedsaber <norhan3182001@gamil.com>
2023-03-24 00:45:37 -0700

    ..

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,8 @@ for (i = 0; i < 10; i++)
 {
 for (j = 0; j < 10; j++)
 {
-k = j * i;if (j == 0)
+k = j * i;
+if (j == 0)
 {
 _putchar(k + '0');
 }
@@ -25,7 +26,8 @@ _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(k + '0');
-} else if (k >= 10)
+}
+else if (k >= 10)
 {
 _putchar(',');
 _putchar(' ');
