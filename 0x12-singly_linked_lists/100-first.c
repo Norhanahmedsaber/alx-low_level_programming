762c6d64122889f443d5724590de7b5f94e776f7
Norhanahmedsaber <norhan3182001@gamil.com>
2023-04-28 08:45:11 -0700

    ..

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * premain - function executed before main
+ * Return: no return.
+ */
+
+void __attribute__ ((constructor)) premain()
+{
+printf("You're beat! and yet, you must allow");
+printf(",\nI bore my house upon my back!\n");
+}
